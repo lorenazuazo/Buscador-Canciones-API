a54163de3d58745a67bfba542a13d25c27099ad4
Lorena Zuazo <pepa_zz@hotmail.com>
2021-01-04 16:48:44 -0300

    buscador canciones

---
+++ js/api.js
@@ -0,0 +1,31 @@
+import * as UI from './interfaz.js';
+
+class API{
+    constructor(artista,cancion){
+        this.artista = artista;
+        this.cancion = cancion
+    }
+
+    consultarAPI(){
+        const url = `https://api.lyrics.ovh/v1/${this.artista}/${this.cancion}`;
+
+        fetch(url)
+            .then(respuesta => respuesta.json())
+            .then(resultado =>{
+                if(resultado.lyrics){
+                    const { lyrics } = resultado;
+                    UI.divResulatdo.textContent = lyrics;
+                    UI.headingResultado.textContent = `Letra de la cancion: ${this.cancion} de ${this.artista}`;
+                }else{
+                    UI.divMensajes.textContent = 'Cancion no encontrada';
+                    UI.divMensajes.classList.add('error');
+
+                    setTimeout(() => {
+                        UI.divMensajes.remove();
+                    }, 3000);
+                }
+            })
+    }
+}
+
+export default API;
---
+++ js/interfaz.js
@@ -0,0 +1,5 @@
+export const formularioBuscar = document.querySelector('#formulario-buscar'),
+             divBuscar = document.querySelector('#buscar'),
+             divMensajes = document.querySelector('#mensajes'),
+             divResulatdo = document.querySelector('#resultado'),
+             headingResultado = document.querySelector('.letra-resultado h2');
